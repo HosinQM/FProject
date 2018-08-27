c29f4a08f4baf950e0071ec8e3cb337e6c977387
Hosin QM <32760218+HosinQM@users.noreply.github.com>
2018-08-27 04:32:25 +0430

    update +0.5

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'pug')
+
+app.use(express.static(`${__dirname}/public`))
+
 app.get('/', (req, res) => {
-    res.sendfile(path.join(__dirname + '/index.html'))
+    res.render('index')
+})
+
+app.get('/signin', (req, res) => {
+    res.render('signin')
 })
 
 app.listen(port, () => {
